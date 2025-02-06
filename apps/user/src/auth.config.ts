457195f9b88c4f7a3603c012a6413c34b0c7069b
Aashim <thelastmanarmy37@gmail.com>
2025-02-07 00:18:23 +0545

    added the workflows

--- apps/user/src/auth.config.ts
+++ apps/user/src/auth.config.ts
@@ -1,37 +1,37 @@
 import type { NextAuthConfig } from "next-auth";
-import { loginSchema } from "@repo/common/user";
-import { getUserByEmail } from "@repo/db/user";
+// import { loginSchema } from "@repo/common/user";
+// import { getUserByEmail } from "@repo/db/user";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import bcrypt from "bcryptjs";
-import Credentials from "next-auth/providers/credentials";
+// import bcrypt from "bcryptjs";
+// import Credentials from "next-auth/providers/credentials";
 // Notice this is only an object, not a full Auth.js instance
 //This is used in the edge runtime for middleware
 export default {
-	providers: [
-		GitHubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		}),
-		Credentials({
-			name: "Sign in with Email and Password",
-			async authorize(credentials) {
-				const refinedFields = loginSchema.safeParse(credentials);
-				if (refinedFields.success) {
-					const user = await getUserByEmail(refinedFields.data.email);
-					if (!user || !user.password) return null; //say for o-auth user they won't have password so they shouldn't be allowed to login with credentials
-					const passwordMatch = await bcrypt.compare(
-						refinedFields.data.password,
-						user.password
-					);
-					if (passwordMatch) return user;
-				}
-				return null;
-			},
-		}),
-	],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // Credentials({
+    //   name: "Sign in with Email and Password",
+    //   async authorize(credentials) {
+    //     const refinedFields = loginSchema.safeParse(credentials);
+    //     if (refinedFields.success) {
+    //       const user = await getUserByEmail(refinedFields.data.email);
+    //       if (!user || !user.password) return null; //say for o-auth user they won't have password so they shouldn't be allowed to login with credentials
+    //       const passwordMatch = await bcrypt.compare(
+    //         user.password,
+    //         refinedFields.data.password
+    //       );
+    //       if (passwordMatch) return user;
+    //     }
+    //     return null;
+    //   },
+    // }),
+  ],
 } satisfies NextAuthConfig;
--- apps/user/src/auth.ts
+++ apps/user/src/auth.ts
@@ -2,60 +2,82 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@repo/db/client";
 import authConfig from "@/auth.config";
-import { getUserById } from "@repo/db/user";
+import { getUserByEmail, getUserById } from "@repo/db/user";
 import { get2FAConfirmationByUserId } from "@repo/db/getVerificationByEmail";
+import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "@repo/common/user";
+import bcrypt from "bcryptjs";
 export const { handlers, auth, signIn, signOut } = NextAuth({
-	events: {
-		//events are something that doesn't return response but useful for logs , handle side effects like in our case.
-		//this gets trigger when we use one of the provider like google, github,twitter so we directly manipulated the emailVerified to be true
-		async linkAccount({ user }) {
-			await prisma.user.update({
-				where: { id: user.id },
-				data: {
-					emailVerified: new Date(),
-				},
-			});
-		},
-	},
-	pages: {
-		signIn: "/login",
-		error: "/error",
-	},
-	callbacks: {
-		async signIn({ user, account }) {
-			if (account?.provider !== "credentials") return true;
-			if (!user.id) return false;
-			const existingUser = await getUserById(user.id);
-			if (!existingUser?.emailVerified) return false;
-			if (existingUser.isEnabled2FA) {
-				const isConfirmed = await get2FAConfirmationByUserId(existingUser.id);
-				if (!isConfirmed) return false;
-				await prisma.twoFactorConfirmation.delete({
-					where: { id: isConfirmed.id },
-				});
-			}
-			return true;
-		},
-		async session({ token, session }) {
-			if (token.sub && session.user) {
-				session.user.id = token.sub; //adding id
-			}
-			if (token.role && session.user) {
-				session.user.role = token.role;
-			}
-			// console.log("Session", session);
-			return session;
-		},
-		async jwt({ token }) {
-			if (!token.sub) return token;
-			const existingUser = await getUserById(token.sub);
-			if (!existingUser) return token;
-			token.role = existingUser.role;
-			// console.log("Token", token); //^so we have token.sub with the user id now to pass it to session
-			return token;
-		},
-	},
-	adapter: PrismaAdapter(prisma),
-	session: { strategy: "jwt" },
-	...authConfig,
+  events: {
+    //events are something that doesn't return response but useful for logs , handle side effects like in our case.
+    //this gets trigger when we use one of the provider like google, github,twitter so we directly manipulated the emailVerified to be true
+    async linkAccount({ user }) {
+      await prisma.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/error",
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+      if (!user.id) return false;
+      const existingUser = await getUserById(user.id);
+      if (!existingUser?.emailVerified) return false;
+      if (existingUser.isEnabled2FA) {
+        const isConfirmed = await get2FAConfirmationByUserId(existingUser.id);
+        if (!isConfirmed) return false;
+        await prisma.twoFactorConfirmation.delete({
+          where: { id: isConfirmed.id },
+        });
+      }
+      return true;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub; //adding id
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      // console.log("Session", session);
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      // console.log("Token", token); //^so we have token.sub with the user id now to pass it to session
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+  providers: [
+    ...authConfig.providers,
+    Credentials({
+      name: "Sign in with Email and Password",
+      async authorize(credentials) {
+        const refinedFields = loginSchema.safeParse(credentials);
+        if (refinedFields.success) {
+          const user = await getUserByEmail(refinedFields.data.email);
+          if (!user || !user.password) return null; //say for o-auth user they won't have password so they shouldn't be allowed to login with credentials
+          const passwordMatch = await bcrypt.compare(
+            refinedFields.data.password,
+            user.password
+          );
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
 });
